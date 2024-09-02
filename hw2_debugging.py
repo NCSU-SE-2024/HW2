d9e773428ea722d939bd2c07884d56283712f754
Anish Dhage <anish.dhage@gmail.com>
2024-09-02 19:57:14 -0400

    MergeSort file bug fixes

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -1,8 +1,12 @@
 """
 This module performs a merge sort on an array that is randomly populated using a shell command.
 """
+
+import logging
 import rand
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
 
 def merge_sort(input_arr):
     """
@@ -14,13 +18,14 @@ def merge_sort(input_arr):
     Returns:
         list: The sorted list.
     """
+    logger.info(input_arr)
     if len(input_arr) == 1:
         return input_arr
 
     half = len(input_arr) // 2
+    logger.info(half)
 
-    return recombine(merge_sort(
-        input_arr[:half]), merge_sort(input_arr[half:]))
+    return recombine(merge_sort(input_arr[:half]), merge_sort(input_arr[half:]))
 
 
 def recombine(left_arr, right_arr):
@@ -36,25 +41,30 @@ def recombine(left_arr, right_arr):
     """
     left_index = 0
     right_index = 0
+    print("left Arr: ", left_arr)
+    print("right Arr: ", right_arr)
     merge_arr = [None] * (len(left_arr) + len(right_arr))
     while left_index < len(left_arr) and right_index < len(right_arr):
         if left_arr[left_index] < right_arr[right_index]:
-            right_index += 1
             merge_arr[left_index + right_index] = left_arr[left_index]
-        else:
             left_index += 1
+        else:
             merge_arr[left_index + right_index] = right_arr[right_index]
+            right_index += 1
 
-    for i in range(right_index, len(right_arr)):
-        merge_arr[left_index + right_index] = right_arr[i]
+    if right_index < len(right_arr):
+        for i in range(right_index, len(right_arr)):
+            merge_arr[left_index + i] = right_arr[i]
 
-    for i in range(left_index, len(left_arr)):
-        merge_arr[left_index + right_index] = left_arr[i]
+    if left_index < len(left_arr):
+        for i in range(left_index, len(left_arr)):
+            merge_arr[i + right_index] = left_arr[i]
 
+    print("Merge Arr: ", merge_arr)
     return merge_arr
 
-
-arr = rand.random_array([None] * 20)
+arr = rand.random_array([None]*20)
+logger.info(arr)
 arr_out = merge_sort(arr)
 
 print(arr_out)
