3ffeb93e752e8ff2623ff2c5cb68f7199e86ff73
Gaurav Sheth <gauravsheth2001@gmail.com>
2024-09-02 18:15:47 -0400

    formatted debugging.py and hw2_debugging.py according to pylint, pyright and autopep8

--- debugging.py
+++ debugging.py
@@ -1,21 +1,34 @@
-def selectionSort(arr):
-    n = len(arr)
-    for i in range(n):
-        lowestIndex = i
-        for j in range(n):
-            if arr[j] < arr[lowestIndex]:
-                lowestIndex = j
+"""
+This module provides an implementation of the selection sort algorithm.
+"""
 
-        arr[lowestIndex] = arr[i]
-        arr[i] = arr[lowestIndex]
-        n = n - 1
+
+def selection_sort(arr):
+    """
+    Sorts an array in ascending order using the selection sort algorithm.
+
+    Args:
+        input_array (list): The list of elements to be sorted.
+
+    Returns:
+        list: The sorted list.
+    """
+    length_arr = len(arr)
+    for i in range(length_arr):
+        lowest_index = i
+        for j in range(length_arr):
+            if arr[j] < arr[lowest_index]:
+                lowest_index = j
+
+        arr[lowest_index] = arr[i]
+        arr[i] = arr[lowest_index]
+        length_arr = length_arr - 1
 
     return arr
 
+
 arr_in = [5, 3, 2, 1, 8, 10, 11, 9, 23]
 
-arr_out = selectionSort(arr_in)
+arr_out = selection_sort(arr_in)
 
 print(arr_out)
-
-
